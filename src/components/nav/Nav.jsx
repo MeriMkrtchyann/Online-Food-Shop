e079b3ce9be28cb101ceb62901c33ede3f58745d
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2024-03-14 16:28:52 +0400

    add in the nav category and pyramid menu icon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,26 +1,37 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faList } from '@fortawesome/free-solid-svg-icons'
 import "./Nav.css"
 
 export default function Nav(){
     return(
-        <nav>
+        <nav className='nav'>
             <div className="logo">
-                <img src ={process.env.PUBLIC_URL + "img/logo.svg"} alt="logo"/>
+                <img src ={process.env.PUBLIC_URL + "img/logo.svg"} alt="logo"/> 
             </div>
-            <div className="categories">
-                <ul>
-                    <li>Hot dishes</li>
-                    <li>Fresh rolls</li>
-                    <li>Salads</li>
-                    <li>Sushi</li>
-                    <li>Burgers</li>
-                    <li>Desserts</li>
-                </ul>
+            <div className="conteyner-categories-list-user">
+                <div className="categories">
+                    <ul>
+                        <li> <a href="#">Hot dishes</a></li>
+                        <li> <a href="#">Fresh rolls</a></li>
+                        <li> <a href="#">Salads</a></li>
+                        <li> <a href="#">Sushi</a></li>
+                        <li> <a href="#">Burgers</a></li>
+                        <li> <a href="#">Desserts</a></li>
+                    </ul>
+                </div>
+                <div class="cadegori-list">
+                    <button>
+                        <FontAwesomeIcon icon={faList} className="fa-light fa-List" />
+                    </button>
+                </div>
+                <div className='user'>
+                    <button>
+                        <FontAwesomeIcon icon={faUser} className="fa-light fa-user" />
+                    </button>
+                </div>
             </div>
-            <div className='user'>
-                <FontAwesomeIcon icon={faUser} className="fa-light fa-user" />
-            </div>
+
         </nav>
     )
 }
