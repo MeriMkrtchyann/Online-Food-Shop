982282c8dfe62470daa8b43cc633a7fcf0415f85
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2024-03-14 17:04:53 +0400

    add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import HomePage from "./pages/HomePage";
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+    <>
+      <Routes>
+        <Route path="login" elemt={<HomePage/>}/>
+        <Route path="registration" elemt={<HomePage/>}/>
+      </Routes>
+      <Routes>
+          <Route path="/" element={<HomePage/>} />
+          {/* <Route path="/rolls" element={ } />
+          <Route path="/salads" element={ } />
+          <Route path="/sushi" element={ } />
+          <Route path="/burgers" element={ } />
+          <Route path="/desserts" element={ } /> */}
+      </Routes>
+    </>
   );
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faList } from '@fortawesome/free-solid-svg-icons'
@@ -7,31 +8,30 @@ export default function Nav(){
     return(
         <nav className='nav'>
             <div className="logo">
-                <img src ={process.env.PUBLIC_URL + "img/logo.svg"} alt="logo"/> 
+                <Link to = "/">
+                    <img src ={process.env.PUBLIC_URL + "img/logo.svg"} alt="logo"/> 
+                </Link>
             </div>
             <div className="conteyner-categories-list-user">
                 <div className="categories">
-                    <ul>
-                        <li> <a href="#">Hot dishes</a></li>
-                        <li> <a href="#">Fresh rolls</a></li>
-                        <li> <a href="#">Salads</a></li>
-                        <li> <a href="#">Sushi</a></li>
-                        <li> <a href="#">Burgers</a></li>
-                        <li> <a href="#">Desserts</a></li>
-                    </ul>
+                    <Link to = "/">Hot dishes</Link>
+                    <Link to = "/">Fresh rolls</Link>
+                    <Link to = "/">Salads</Link>
+                    <Link to = "/">Sushi</Link>
+                    <Link to = "/">Burgers</Link>
+                    <Link to = "/">Desserts</Link>
                 </div>
                 <div class="cadegori-list">
-                    <button>
+                    <Link to = "/">
                         <FontAwesomeIcon icon={faList} className="fa-light fa-List" />
-                    </button>
+                    </Link>
                 </div>
                 <div className='user'>
-                    <button>
+                    <Link to = "/">
                         <FontAwesomeIcon icon={faUser} className="fa-light fa-user" />
-                    </button>
+                    </Link>
                 </div>
             </div>
-
         </nav>
     )
 }
