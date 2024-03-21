5c8a80c2f6dd8228187ef33b257810674b7f8cc2
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2024-03-21 11:45:27 +0400

    add toggle password visibility in UserPassword

--- src/components/validations/UserPasswordValidation.jsx
+++ src/components/validations/UserPasswordValidation.jsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import FormHelperText from '@mui/material/FormHelperText';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function UserPasswordValidation({ aboutUser, aboutUserPassword, setAboutUser, setPassword = "" ,password }){
 
+    const [showPassword, setShowPassword] = React.useState(false)
     const passwordValidationRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[\w@$!%*?&.\\]{8,}$/;
     let isPasswordInvalid =  !password.match(passwordValidationRegex) && (password.length >= 1 && password.length <= 7)
-
+    
     const handlePasswordChange  = ( event ) => {
             const password = event.target.value
             setPassword(password)
@@ -29,6 +34,10 @@ export default function UserPasswordValidation({ aboutUser, aboutUserPassword, s
                 })
             }
     }
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
    
     return (
         <Grid item xs={12} style={{ paddingTop: 5 }}>
@@ -37,13 +46,26 @@ export default function UserPasswordValidation({ aboutUser, aboutUserPassword, s
                 fullWidth
                 id="password"
                 label="Password"
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 name="password"
                 autoComplete="newPassword"
-                valueemail
                 value={password}
                 onChange={handlePasswordChange}
                 error={isPasswordInvalid}
+                InputProps={{
+                endAdornment: (
+                <InputAdornment position="end">
+                    <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={togglePasswordVisibility}
+                    edge="end"
+                    style={{marginRight: 2}}
+                    >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                </InputAdornment>
+                ),
+             }}
             />
              {isPasswordInvalid ? (
                 <FormHelperText error>
