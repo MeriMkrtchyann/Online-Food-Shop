df4f7d6d2f9daa26dd6aa694920de87baa959399
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2024-03-25 17:42:23 +0400

    add heshing for password sha256

--- src/services/firebaseSet.js
+++ src/services/firebaseSet.js
@@ -2,6 +2,7 @@ import { getDatabase, ref, set } from "firebase/database";
 import { v4 as uuidv4 } from 'uuid';
 
 export default async function writeUserData(aboutUser) {
+    const sha256 = require('js-sha256')
     const db = getDatabase();
     const userId = uuidv4()
     try{
@@ -12,6 +13,7 @@ export default async function writeUserData(aboutUser) {
             email : aboutUser.aboutUserEmail.value,
             conatct : aboutUser.aboutUserPhome.value,
             addres : aboutUser.aboutUserAddres.value,
+            password : sha256(aboutUser.aboutUserPassword.value)
         });
     }catch(e){
         throw new Error("")
